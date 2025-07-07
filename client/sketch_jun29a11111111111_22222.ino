1cf37579b99b619434dcf6fb26b891528c03afb6
peterwolf-pl <84736182+peterwolf-pl@users.noreply.github.com>
2025-07-08 00:07:44 +0200

    Add files via upload

--- client/sketch_jun29a11111111111_22222.ino
+++ client/sketch_jun29a11111111111_22222.ino
@@ -2,8 +2,12 @@
 #include <HTTPClient.h>
 #include "HX711.h"
 #include <Update.h>
+#include <ArduinoJson.h>
 
 const char* firmware_url = "http://pszczol.one.pl/firmware/esp32.bin";
+const char* config_url = "http://pszczol.one.pl/settings.json";
+const char* report_url = "http://pszczol.one.pl/api/report_update.php";
+const char* change_url = "http://pszczol.one.pl/api/report_change.php";
 const char* ssid = "AirPortExtreme";
 const char* password = "Flash255";
 const char* serverName = "http://pszczol.one.pl/api/add.php/";
@@ -13,16 +17,28 @@ HX711 scale;
 uint8_t dataPin = 16;
 uint8_t clockPin = 4;
 float previous = 0;
+bool firmwareUpdateFlag = false;
+int loopDelay = 10;
+float offsetVal = -598696;
+float scaleVal = -25.353687;
 
-void checkFirmwareUpdate() {
+bool prevFirmwareUpdate = false;
+int prevLoopDelay = 10;
+float prevOffsetVal = -598696;
+float prevScaleVal = -25.353687;
+
+void reportUpdate(bool success);
+void reportChange();
+
+bool checkFirmwareUpdate() {
   WiFiClient client;
   HTTPClient http;
- Serial.println(" ");
+  Serial.println(" ");
   Serial.println("Sprawdzanie aktualizacji...");
   http.begin(client, firmware_url);
 
-
   int httpCode = http.GET();
+  bool success = false;
 
   if (httpCode == 200) {
     int len = http.getSize();
@@ -32,22 +48,81 @@ void checkFirmwareUpdate() {
       size_t written = Update.writeStream(*stream);
 
       if (written == len && Update.end()) {
-         Serial.println(" ");
+        Serial.println(" ");
         Serial.println("Aktualizacja zakończona sukcesem, restartuję...");
-        ESP.restart();
+        success = true;
       } else {
         Serial.println("Błąd aktualizacji: " + String(Update.getError()));
       }
     } else {
       Serial.println("Nie można rozpocząć aktualizacji.");
-    
     }
   } else {
     Serial.println("Brak aktualizacji. ");
-     Serial.println(" ");
+    Serial.println(" ");
   }
-http.end();
-   Serial.println(" ");
+  http.end();
+  reportUpdate(success);
+  if (success) {
+    ESP.restart();
+  }
+  return success;
+}
+
+bool fetchConfig() {
+  HTTPClient http;
+  http.begin(config_url);
+  int httpCode = http.GET();
+  bool triggerUpdate = false;
+  if (httpCode == 200) {
+    String payload = http.getString();
+    StaticJsonDocument<256> doc;
+    DeserializationError err = deserializeJson(doc, payload);
+    if (!err) {
+      firmwareUpdateFlag = doc["firmwareUpdate"];
+      loopDelay = doc["loopDelay"];
+      offsetVal = doc["offset"];
+      scaleVal = doc["scale"];
+    }
+  }
+  http.end();
+
+  if (firmwareUpdateFlag != prevFirmwareUpdate ||
+      loopDelay != prevLoopDelay ||
+      offsetVal != prevOffsetVal ||
+      scaleVal != prevScaleVal) {
+    Serial.println("CONFIG CHANGE DETECTED");
+    reportChange();
+    scale.set_offset(offsetVal);
+    scale.set_scale(scaleVal);
+    triggerUpdate = (!prevFirmwareUpdate && firmwareUpdateFlag);
+    prevFirmwareUpdate = firmwareUpdateFlag;
+    prevLoopDelay = loopDelay;
+    prevOffsetVal = offsetVal;
+    prevScaleVal = scaleVal;
+  }
+  return triggerUpdate;
+}
+
+void reportUpdate(bool success) {
+  HTTPClient http;
+  http.begin(report_url);
+  http.addHeader("Content-Type", "application/json");
+  String payload = String("{\"success\":") + (success ? "true" : "false") + "}";
+  http.POST(payload);
+  http.end();
+}
+
+void reportChange() {
+  HTTPClient http;
+  http.begin(change_url);
+  http.addHeader("Content-Type", "application/json");
+  String payload = String("{\"firmwareUpdate\":") + (firmwareUpdateFlag ? "true" : "false") +
+                    ",\"loopDelay\":" + String(loopDelay) +
+                    ",\"offset\":" + String(offsetVal) +
+                    ",\"scale\":" + String(scaleVal) + "}";
+  http.POST(payload);
+  http.end();
 }
 
 void setup() {
@@ -65,11 +140,14 @@ void setup() {
  Serial.println(" ");
 
   scale.begin(dataPin, clockPin);
-  scale.set_offset(-598696);
-  scale.set_scale(-25.353687);
+  scale.set_offset(offsetVal);
+  scale.set_scale(scaleVal);
   scale.tare();
 
-  
+  bool doUpdateStartup = fetchConfig();
+  if (doUpdateStartup) {
+    checkFirmwareUpdate();
+  }
 }
 
 void loop() {
@@ -79,6 +157,11 @@ void loop() {
     WiFi.reconnect();
     delay(5000);
     return;
+  }
+  bool doUpdate = fetchConfig();
+
+  if (doUpdate) {
+    checkFirmwareUpdate();
   }
 
   float weight = getStableWeight();
@@ -107,10 +190,8 @@ void loop() {
 //  Serial.println(" ");
 //  https.end();
  delay(600);
-http.end();
- delay(6000*5);
- checkFirmwareUpdate();
- delay(6000*5*5);
+  http.end();
+  delay(loopDelay * 1000);
 }
 
 float getStableWeight() {
